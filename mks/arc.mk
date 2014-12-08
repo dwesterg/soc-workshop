87012cb493caad033a92e12dfaafab8474c35429
Dalon Westergreen <dwesterg@gmail.com>
2014-12-08 15:50:59 -0800

    add coreutils build b/c arc is old

--- mks/arc.mk
+++ mks/arc.mk
@@ -1,6 +1,29 @@
 TAR_SOURCE := tar-1.28.tar.gz
 TAR_SOURCE_PACKAGE := "http://ftp.gnu.org/gnu/tar/$(TAR_SOURCE)"
 
+COREUTILS_SOURCE := coreutils-8.23.tar.xz
+COREUTILS_SOURCE_TAR := coreutils-8.23.tar
+COREUTILS_SOURCE_PACKAGE := "http://ftp.gnu.org/gnu/coreutils/$(COREUTILS_SOURCE)"
+
+.PHONY: coreutils.download
+coreutils.download: $(DL)/$(COREUTILS_SOURCE)
+$(DL)/$(COREUTILS_SOURCE):
+	$(MKDIR) $(DL)
+	wget -O $@ $(COREUTILS_SOURCE_PACKAGE)
+
+$(call get_stamp_target,coreutils.extract): $(DL)/$(COREUTILS_SOURCE)
+	$(CAT) $< | $(XZ) -d > $(DL)/$(COREUTILS_SOURCE_TAR)
+	$(MKDIR) coreutils
+	$(TAR) -xvf $(DL)/$(COREUTILS_SOURCE_TAR) --strip-components 1 -C coreutils
+	$(stamp_target)
+
+.PHONY: coreutils.compile
+coreutils.compile: $(call get_stamp_target,coreutils.compile)
+$(call get_stamp_target,coreutils.compile): $(call get_stamp_target,coreutils.extract)
+	$(shell cd coreutils;./configure > /dev/null)
+	$(MAKE) -C coreutils all
+	$(stamp_target)
+
 .PHONY: tar.download
 tar.download: $(DL)/$(TAR_SOURCE)
 $(DL)/$(TAR_SOURCE):
@@ -26,7 +49,7 @@ arc_build_all:
 	$(MAKE)  http_proxy=$(HTTP_PROXY) https_proxy=$(HTTPS_PROXY) tar.compile
 	$(MAKE) TAR=$(CURDIR)/tar/src/tar http_proxy=$(HTTP_PROXY) https_proxy=$(HTTPS_PROXY) downloads
 	$(MAKE) TAR=$(CURDIR)/tar/src/tar $(ARC_BUILD_INTERMEDIATE_TARGETS)
-	$(MAKE) TAR=$(CURDIR)/tar/src/tar all
+	$(MAKE) PATH=$(CURDIR)/coreutils/src:$(PATH) TAR=$(CURDIR)/tar/src/tar all
 
 	
 define arc_build_project
