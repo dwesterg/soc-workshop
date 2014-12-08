551d5d6d52dccc3693954a0eb4651589a6bad5ac
Dalon Westergreen <dwesterg@gmail.com>
2014-12-08 15:26:36 -0800

    add updated tar compile to arc build b/c arc is so darn old

--- mks/arc.mk
+++ mks/arc.mk
@@ -1,11 +1,31 @@
+TAR_SOURCE := tar-1.28.tar.gz
+TAR_SOURCE_PACKAGE := "http://ftp.gnu.org/gnu/tar/$(TAR_SOURCE)"
+
+.PHONY: tar.download
+tar.download: $(DL)/$(TAR_SOURCE)
+$(DL)/$(TAR_SOURCE):
+	$(MKDIR) $(DL)
+	wget -O $@ $(TAR_SOURCE_PACKAGE)
+
+$(call get_stamp_target,tar.extract): $(DL)/$(TAR_SOURCE)
+	$(MKDIR) tar
+	$(TAR) -xzvf $< --strip-components 1 -C tar
+	$(stamp_target)
+
+.PHONY: tar.compile
+tar.compile: $(call get_stamp_target,tar.compile)
+$(call get_stamp_target,tar.compile): $(call get_stamp_target,tar.extract)
+	$(shell cd tar;./configure > /dev/null)
+	$(MAKE) -C tar all
+	$(stamp_target)
 
 ARC_BUILD_INTERMEDIATE_TARGETS := $(foreach r,$(REVISION_LIST),arc_build-$r)
 
 .PHONY: arc_build_all
-arc_build_all:
-	$(MAKE) http_proxy=$(HTTP_PROXY) https_proxy=$(HTTPS_PROXY) downloads
-	$(MAKE) $(ARC_BUILD_INTERMEDIATE_TARGETS)
-	$(MAKE) all
+arc_build_all: $(call get_stamp_target,tar.compile)
+	$(MAKE) TAR=$(CURDIR)/tar/src/tar http_proxy=$(HTTP_PROXY) https_proxy=$(HTTPS_PROXY) downloads
+	$(MAKE) TAR=$(CURDIR)/tar/src/tar $(ARC_BUILD_INTERMEDIATE_TARGETS)
+	$(MAKE) TAR=$(CURDIR)/tar/src/tar all
 
 	
 define arc_build_project
