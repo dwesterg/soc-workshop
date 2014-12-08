f067811cc89e8478ee7d08eb074c369a6bad806b
Dalon Westergreen <dwesterg@gmail.com>
2014-12-08 12:17:10 -0800

    add

--- mks/arc.mk
+++ mks/arc.mk
@@ -3,11 +3,12 @@ ARC_BUILD_INTERMEDIATE_TARGETS := $(foreach r,$(REVISION_LIST),arc_build-$r)
 
 ARC_PARALLEL := $(words $(ARC_BUILD_INTERMEDIATE_TARGETS))
 
+ARC_PARALLEL_ADD := $(shell echo ${ARC_PARALLEL}+1 | bc)
+
 .PHONY: arc_build_all
 arc_build_all:
-	make http_proxy=$(HTTP_PROXY) https_proxy=$(HTTPS_PROXY) downloads
-	make -j$(ARC_PARALLEL) create_all_projects
-	make arc_build_init
+	make -j$(ARC_PARALLEL_ADD) http_proxy=$(HTTP_PROXY) https_proxy=$(HTTPS_PROXY) downloads create_all_projects
+#	make -j$(ARC_PARALLEL) create_all_projects
 	make -j$(ARC_PARALLEL) $(ARC_BUILD_INTERMEDIATE_TARGETS)
 	make -j8 all
 
