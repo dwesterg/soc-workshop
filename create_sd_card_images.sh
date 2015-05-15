c8ef7ac0e40878911caa81230f2fd252e48727dc
Rod Frazer <rfrazer@altera.com>
2015-05-14 22:06:50 -0400

    added better error handling and detection for no partitions after fdisk

--- create_sd_card_images.sh
+++ create_sd_card_images.sh
@@ -4,7 +4,11 @@ PROGRAM_NAME="$(basename ${0})"
 THE_SD_FAT_TAR_GZ=
 DEV_BOARD_TYPE=
 
-# parse the command line arguments
+###############################################################################
+#
+# Parse the command line arguments
+#
+###############################################################################
 for i in "$@"
 do
 case $i in
@@ -21,6 +25,8 @@ case $i in
 		echo "	CRITICALLINK_MITYSOM_DEVKIT"
 		echo "	DE0_NANO_SOC"
 		echo "	MACNICA_HELIO_14"
+		echo ""
+		echo "Must be run as root user, or 'sudo'."
 		echo ""
 		exit 1
 	;;
@@ -158,15 +164,34 @@ MY_TMP_TAR="$(mktemp --tmpdir=. --directory TMP_TAR.XXXX)"
 # Verify that none of our working files or directories already exist
 #
 ###############################################################################
+
+###############################################################################
+#
+# Build our output file name
+#
+###############################################################################
 MY_SD_CARD_IMAGE="${THE_SD_FAT_TAR_GZ#*.}"
 MY_SD_CARD_IMAGE="${MY_SD_CARD_IMAGE%%.*}"
 MY_SD_CARD_IMAGE="sd_card.${DEV_BOARD_TYPE}.${MY_SD_CARD_IMAGE}.image"
 
+###############################################################################
+#
+# Echo what we're doing from here on...
+#
+###############################################################################
 echo "Creating SD card image file filled with 0xFF pattern."
-dd if=/dev/zero bs=1M count=${MY_SD_CARD_IMAGE_1M_BLOCKS} 2> /dev/null | tr '\000' '\377' > ${MY_SD_CARD_IMAGE} || { echo "ERROR"; exit 1; }
+dd if=/dev/zero bs=1M count=${MY_SD_CARD_IMAGE_1M_BLOCKS} 2> /dev/null | tr '\000' '\377' > ${MY_SD_CARD_IMAGE} || { 
+	echo "ERROR"
+	rm -Rf ${MY_SD_FAT_MNT} ${MY_TMP_TAR}
+	exit 1
+}
 
 echo "Attaching SD card image file to loop device."
-MY_LOOP_DEV=$(losetup --show -f ${MY_SD_CARD_IMAGE}) || { echo "ERROR"; exit 1; }
+MY_LOOP_DEV=$(losetup --show -f ${MY_SD_CARD_IMAGE}) || { 
+	echo "ERROR"
+	rm -Rf ${MY_SD_FAT_MNT} ${MY_TMP_TAR}
+	exit 1
+}
 
 echo "Creating partition table in MBR of SD card image file."
 fdisk ${MY_LOOP_DEV} <<EOF > /dev/null 2>&1
@@ -179,7 +204,7 @@ n
 p
 1
 
-+250M
++256M
 n
 p
 2
@@ -198,42 +223,122 @@ w
 EOF
 
 echo "Detaching SD card image file from loop device."
-losetup -d ${MY_LOOP_DEV} || { echo "ERROR"; exit 1; }
+losetup -d ${MY_LOOP_DEV} || { 
+	echo "ERROR"
+	rm -Rf ${MY_SD_FAT_MNT} ${MY_TMP_TAR}
+	exit 1
+}
 
 echo "Attaching SD card image file to loop device with partition scan."
-MY_LOOP_DEV=$(losetup --show -f --partscan ${MY_SD_CARD_IMAGE}) || { echo "ERROR"; exit 1; }
+MY_LOOP_DEV=$(losetup --show -f --partscan ${MY_SD_CARD_IMAGE}) || { 
+	echo "ERROR"
+	rm -Rf ${MY_SD_FAT_MNT} ${MY_TMP_TAR}
+	exit 1
+}
+
+echo "Verify loop partition 1 exists."
+[ -b "${MY_LOOP_DEV}p1" ] || {
+	echo "ERROR"
+	losetup -d ${MY_LOOP_DEV}
+	rm -Rf ${MY_SD_FAT_MNT} ${MY_TMP_TAR}
+	exit 1
+}
+
+echo "Verify loop partition 2 exists."
+[ -b "${MY_LOOP_DEV}p2" ] || {
+	echo "ERROR"
+	losetup -d ${MY_LOOP_DEV}
+	rm -Rf ${MY_SD_FAT_MNT} ${MY_TMP_TAR}
+	exit 1
+}
+
+echo "Verify loop partition 3 exists."
+[ -b "${MY_LOOP_DEV}p3" ] || {
+	echo "ERROR"
+	losetup -d ${MY_LOOP_DEV}
+	rm -Rf ${MY_SD_FAT_MNT} ${MY_TMP_TAR}
+	exit 1
+}
 
 echo "Initializing FAT volume in partition 1 of SD card image file."
-mkfs -t vfat -F 32 ${MY_LOOP_DEV}p1 > /dev/null || { echo "ERROR"; exit 1; }
+mkfs -t vfat -F 32 ${MY_LOOP_DEV}p1 > /dev/null || { 
+	echo "ERROR"
+	losetup -d ${MY_LOOP_DEV}
+	rm -Rf ${MY_SD_FAT_MNT} ${MY_TMP_TAR}
+	exit 1
+}
 
 echo "Mounting FAT partition of SD card image file."
-mount ${MY_LOOP_DEV}p1 ${MY_SD_FAT_MNT} || { echo "ERROR"; exit 1; }
+mount ${MY_LOOP_DEV}p1 ${MY_SD_FAT_MNT} || { 
+	echo "ERROR"
+	losetup -d ${MY_LOOP_DEV}
+	rm -Rf ${MY_SD_FAT_MNT} ${MY_TMP_TAR}
+	exit 1
+}
 
 echo "Extracting sd_fat.tar.gz."
-tar -C ${MY_TMP_TAR} -xf ${THE_SD_FAT_TAR_GZ} || { echo "ERROR"; exit 1; }
+tar -C ${MY_TMP_TAR} -xf ${THE_SD_FAT_TAR_GZ} || { 
+	echo "ERROR"
+	umount ${MY_SD_FAT_MNT}
+	losetup -d ${MY_LOOP_DEV}
+	rm -Rf ${MY_SD_FAT_MNT} ${MY_TMP_TAR}
+	exit 1
+}
 
 echo "Copying into FAT partition."
-cp -R ${MY_TMP_TAR}/* ${MY_SD_FAT_MNT} || { echo "ERROR"; exit 1; }
+cp -R ${MY_TMP_TAR}/* ${MY_SD_FAT_MNT} || { 
+	echo "ERROR"
+	umount ${MY_SD_FAT_MNT}
+	losetup -d ${MY_LOOP_DEV}
+	rm -Rf ${MY_SD_FAT_MNT} ${MY_TMP_TAR}
+	exit 1
+}
 
 echo "Copying preloader image into partition 3 of SD card image file."
 [ -f "${MY_SD_FAT_MNT}/${DEV_BOARD_TYPE}/preloader-mkpimage.bin" ] || {
 	echo "ERROR: could not locate preloader image file"
+	umount ${MY_SD_FAT_MNT}
+	losetup -d ${MY_LOOP_DEV}
+	rm -Rf ${MY_SD_FAT_MNT} ${MY_TMP_TAR}
 	exit 1
 }
-dd if="${MY_SD_FAT_MNT}/${DEV_BOARD_TYPE}/preloader-mkpimage.bin" of=${MY_LOOP_DEV}p3 2> /dev/null || { echo "ERROR"; exit 1; }
+dd if="${MY_SD_FAT_MNT}/${DEV_BOARD_TYPE}/preloader-mkpimage.bin" of=${MY_LOOP_DEV}p3 2> /dev/null || { 
+	echo "ERROR"
+	umount ${MY_SD_FAT_MNT}
+	losetup -d ${MY_LOOP_DEV}
+	rm -Rf ${MY_SD_FAT_MNT} ${MY_TMP_TAR}
+	exit 1
+}
 
 echo "Unmount the FAT partition."
-umount ${MY_SD_FAT_MNT} || { echo "ERROR"; exit 1; }
+umount ${MY_SD_FAT_MNT} || { 
+	echo "ERROR"
+	losetup -d ${MY_LOOP_DEV}
+	rm -Rf ${MY_SD_FAT_MNT} ${MY_TMP_TAR}
+	exit 1
+}
 
 echo "Initializing ext3 volume in partition 2 of SD card image file."
-mkfs -t ext3 ${MY_LOOP_DEV}p2 > /dev/null 2>&1 || { echo "ERROR"; exit 1; }
+mkfs -t ext3 ${MY_LOOP_DEV}p2 > /dev/null 2>&1 || { 
+	echo "ERROR"
+	losetup -d ${MY_LOOP_DEV}
+	rm -Rf ${MY_SD_FAT_MNT} ${MY_TMP_TAR}
+	exit 1
+}
 
 echo "Detaching SD card image file from loop device."
-losetup -d ${MY_LOOP_DEV} || { echo "ERROR"; exit 1; }
+losetup -d ${MY_LOOP_DEV} || { 
+	echo "ERROR"
+	rm -Rf ${MY_SD_FAT_MNT} ${MY_TMP_TAR}
+	exit 1
+}
 
 echo "Removing temporary working directories."
-rm -Rf ${MY_SD_FAT_MNT} ${MY_TMP_TAR} || { echo "ERROR"; exit 1; }
+rm -Rf ${MY_SD_FAT_MNT} ${MY_TMP_TAR} || { 
+	echo "ERROR"
+	exit 1
+}
 
 echo "SD card image created."
-echo "The file ${MY_SD_CARD_IMAGE} is now ready to be copied onto an SD card."
+echo "'${MY_SD_CARD_IMAGE}' is now ready to be copied onto an SD card."
 
