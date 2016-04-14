6ac0ef76a14cb72926068c2e7bd6217150f6ea6e
Dalon Westergreen <dwesterg@gmail.com>
2016-04-13 20:15:19 -0700

    Add back in CL board

--- CREATE_ALL_SD_CARD_COMPRESSED_IMAGES.sh
+++ CREATE_ALL_SD_CARD_COMPRESSED_IMAGES.sh
@@ -28,10 +28,10 @@
 	echo "ERROR"
 	exit 1	
 }
-# ./create_sd_card_images.sh --compress --sd_fat=$1 --board=CRITICALLINK_MITYSOM_DEVKIT || {
-# 	echo "ERROR"
-# 	exit 1	
-# }
+ ./create_sd_card_images.sh --compress --sd_fat=$1 --board=CRITICALLINK_MITYSOM_DEVKIT || {
+ 	echo "ERROR"
+ 	exit 1	
+ }
 ./create_sd_card_images.sh --compress --sd_fat=$1 --board=DE0_NANO_SOC || {
 	echo "ERROR"
 	exit 1	
